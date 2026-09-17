0b8f81a640b25dfbe8d208ff080d1ca96f84912d
FARIZ <farisrachman213@gmail.com>
2026-09-17 08:18:50 +0700

    update project

--- ptmn2/App.js
+++ ptmn2/App.js
@@ -5,9 +5,14 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Nama : mohamad fariz rachman pratama</Text>
-<Text>Tempat tanggal lahir : Cirebon, 1 maret 2006</Text>
-<Text>Cita cita : Financial freedom</Text>
-<Text>Rencana hidup : setelah kuliah saya berencana membangun usaha laundry, lalu jika laundry sudah berjalan dan banyak pelanggan saya akan mulai ternak ayam broiler untuk bisnis telur dan daging<\Text>
+      <Text>Tempat tanggal lahir : Cirebon, 1 maret 2006</Text>
+      <Text>Cita cita : Financial freedom</Text>
+      <Text>
+        Rencana hidup : setelah kuliah saya berencana membangun usaha laundry,
+        lalu jika laundry sudah berjalan dan banyak pelanggan saya akan mulai
+        ternak ayam broiler untuk bisnis telur dan daging
+      </Text>
+
       <StatusBar style="auto" />
     </View>
   );
